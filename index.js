8f99122f0ec06e323f960fc6df48007ca1c099e5
ㅤReee <rebeccachiari99@gmail.com>
2025-03-19 14:22:18 -0300

    removido identico ao true

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const confirmacaoInicial = confirm("Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?")
 
-if (confirmacaoInicial === true) {
+if (confirmacaoInicial) {
     alert("Ótimo! Nós temos as melhores camas de toda a região!")
 } else if (confirmacaoInicial === false) {
     alert("Que pena! Você parecia ser uma pessoa mais legal")
